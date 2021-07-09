02916f1d5b7660e7925fa0fa87011b2f27e7cbac
Ayush Paudel <69846907+ayush723@users.noreply.github.com>
2021-07-09 15:45:13 +0545

    Update init-tables.go

--- metrorailapi_gin/dbutils/init-tables.go
+++ metrorailapi_gin/dbutils/init-tables.go
@@ -4,7 +4,34 @@ import (
 	"database/sql"
 	"log"
 )
+package dbutils
 
+const train = `
+	CREATE TABLE IF NOT EXISTS train(
+		ID INTEGER PRIAMRY KEY ,
+		DRIVER_NAME VARCHAR(64) NULL,
+		OPERATING_STATUS BOOLEAN
+	)
+`
+const station = `
+	CREATE TABLE IF NOT EXISTS station(
+		ID INTEGER PRIMARY KEY ,
+		NAME VARCHAR(64) NULL,
+		OPENING_TIME TIME NULL,
+		CLOSING_TIME TIME NULL
+	)
+`
+
+const schedule = `
+	CREATE TABLE IF NOT EXISTS schedule(
+		ID INTEGER PRIMARY KEY ,
+		TRAIN_ID INT,
+		STATION_ID INT,
+		ARRIVAL_TIME TIME,
+		FOREIGN KEY (TRAIN_ID) REFERENCES train(ID),
+		FOREIGN KEY (STATION_ID) REFERENCES station(ID)
+	)
+`
 func Initialize(dbDriver *sql.DB) {
 	statement, driverError := dbDriver.Prepare(train)
 	if driverError != nil {
